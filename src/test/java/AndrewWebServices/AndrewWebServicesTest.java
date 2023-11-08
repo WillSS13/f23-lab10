76619535d79cbcc9f20e2f16adb007615a87dc86
Will Squibb <wsquibb@andrew.cmu.edu>
2023-11-08 10:18:20 -0500

    Task 3: Use Mocks

--- src/test/java/AndrewWebServices/AndrewWebServicesTest.java
+++ src/test/java/AndrewWebServices/AndrewWebServicesTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
 public class AndrewWebServicesTest {
     Database database;
     RecSys recommender;
@@ -17,7 +19,7 @@ public class AndrewWebServicesTest {
         // You need to use some mock objects here
         database = new InMemoryDatabase(); // We probably don't want to access our real database...
         recommender = new RecSysStub();
-        promoService = new PromoService();
+        promoService = mock(PromoService.class);
 
         andrewWebService = new AndrewWebServices(database, recommender, promoService);
     }
@@ -38,11 +40,16 @@ public class AndrewWebServicesTest {
     public void testSendEmail() {
         // How should we test sendEmail() when it doesn't have a return value?
         // Hint: is there something from Mockito that seems useful here?
+        andrewWebService.sendPromoEmail("test@example.com");
+        verify(promoService).mailTo("test@example.com");
     }
 
     @Test
     public void testNoSendEmail() {
         // How should we test that no email has been sent in certain situations (like right after logging in)?
         // Hint: is there something from Mockito that seems useful here?
+        andrewWebService.logIn("Scotty", 17214);
+        verify(promoService, never()).mailTo(anyString());
+       
     }
 }
